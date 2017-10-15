57ffc9fb147480426f5d294d0167969bdc12ffda
kevinheavey <heaveykevin2@gmail.com>
2017-10-15 23:49:14 +0100

    Create utils.py

---
+++ crawler/utils.py
@@ -0,0 +1,14 @@
+def headline_attribute_from_line(line):
+    equals_sign_loc = line.find('=')
+    attribute_name = line[equals_sign_loc - 4: equals_sign_loc - 1]
+    attribute_value = int(line[equals_sign_loc+2:equals_sign_loc+4])
+    return {'name':attribute_name, 'value':attribute_value}
+
+def parse_headline_attributes(soup):
+    attribute_dict = {}
+    headline_attribute_script = soup.find_all('script', recursive=False)[1]
+    for line in headline_attribute_script.text.split('\r\n'):
+        if 'point' in line:
+            attr_subdict = headline_attribute_from_line(line)
+            attribute_dict[attr_subdict['name']] = attr_subdict['value']
+    return attribute_dict
