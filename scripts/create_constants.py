1ec3a9a8fc17e46448bd6539bb319958eeb0e799
kevinheavey <heaveykevin2@gmail.com>
2017-10-15 21:31:42 +0100

    Rename and keep previous file

--- scripts/create_constants.py
+++ scripts/create_constants.py
@@ -1,6 +1,12 @@
 import requests
 import json
 from bs4 import BeautifulSoup, SoupStrainer
+from pathlib import Path
+import shutil
+
+CONSTANTS_DIR = Path(__file__).parents[1] / 'data/resources/constants/'
+CURRENT_PATH = CONSTANTS_DIR / 'current.json'
+PREVIOUS_PATH = CONSTANTS_DIR / 'previous.json'
 
 
 def get_all_traits_and_specialities():
@@ -23,11 +29,17 @@ def get_all_constants():
     return constants_dict
 
 def save_constants():
+    path = CURRENT_PATH
     constants_dict = get_all_constants()
-    with open('constants.json', 'w') as f:
+    with open(path, 'w') as f:
         json.dump(constants_dict)
 
-if __name__ == '__main__':
+def update_constants():
+    shutil.move(CURRENT_PATH, PREVIOUS_PATH)
     save_constants()
+
+
+if __name__ == '__main__':
+    update_constants()
 
 
