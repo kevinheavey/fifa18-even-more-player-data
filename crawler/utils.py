0f449a216ad276fd5d86eeddf293b9611cbcabee
kevinheavey <heaveykevin2@gmail.com>
2017-10-16 22:45:29 +0100

    Move convert_currency to utils

--- crawler/utils.py
+++ crawler/utils.py
@@ -1,6 +1,10 @@
 import json
 from pathlib import Path
 
+import numpy as np
+import pandas as pd
+
+
 def headline_attribute_from_line(line):
     equals_sign_loc = line.find('=')
     attribute_name = line[equals_sign_loc - 4: equals_sign_loc - 1]
@@ -21,3 +25,11 @@ def read_constants():
     with open(path, 'r') as f:
         constants = json.load(f)
     return constants
+
+
+def convert_currency(curr_col):
+    without_euro_symbol = curr_col.str[1:]
+    unit_symbol = without_euro_symbol.str[-1]
+    numeric_part = np.where(unit_symbol == '0', 0, without_euro_symbol.str[:-1].pipe(pd.to_numeric))
+    multipliers = unit_symbol.replace({'M':1e6, 'K':1e3}).pipe(pd.to_numeric)
+    return numeric_part * multipliers
